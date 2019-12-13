c747070af8e7682f7b7a973bbfcdd6d42b1d3e2b
r0ck3r008 <naman.arora@tutanota.com>
2019-12-13 18:16:43 -0500

    removed bcrypt

--- src/twitter_phx/mix.exs
+++ src/twitter_phx/mix.exs
@@ -42,10 +42,7 @@ defmodule Twitter.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"},
-      {:comeonin, "~> 5.1"},
-      {:bcrypt_elixir, "~> 2.0"},
-      {:guardian, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"}
     ]
   end
 
